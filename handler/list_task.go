8a8a6b044b8691ffaa3b35e66338da24ec1916a5
shinnkura <n.shinzo1010@icloud.com>
2025-06-03 09:26:39 +0900

    タスク一覧のエンドポイント

---
+++ handler/list_task.go
@@ -0,0 +1,32 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/shinnkura/go_todo_app/entity"
+	"github.com/shinnkura/go_todo_app/store"
+)
+
+type ListTask struct {
+	Service *store.TaskStore
+}
+
+type task struct {
+	ID     entity.TaskID     `json:"id"`
+	Title  string            `json:"title"`
+	Status entity.TaskStatus `json:"status"`
+}
+
+func (lt *ListTask) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	tasks := lt.Service.All()
+	rsp := []task{}
+	for _, t := range tasks {
+		rsp = append(rsp, task{
+			ID:     t.ID,
+			Title:  t.Title,
+			Status: t.Status,
+		})
+	}
+	RespondJSON(ctx, w, rsp, http.StatusOK)
+}
